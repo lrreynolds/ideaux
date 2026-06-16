3d914ecd7036660e4930adaef9faf618d9697a6d
Lou <lrreynolds@me.com>
2026-06-16 08:32:54 -0600

    Initial Commit

---
+++ ideaux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ideaux
+//
+//  Created by LouR on 6/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ideaux/ideauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ideauxApp.swift
+//  ideaux
+//
+//  Created by LouR on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ideauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
